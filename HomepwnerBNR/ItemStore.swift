4b1581e16cd03fe5551e5346d54c4a2fb0c81440
AnatoliyChernyuk <toliatche@gmail.com>
2018-01-03 00:08:48 +0200

    1. Project started with TableViewController 2. Item class created as a basic unit for populating the table 3. ItemStore class created to contain the the Item instances

---
+++ HomepwnerBNR/ItemStore.swift
@@ -0,0 +1,25 @@
+//
+//  ItemStore.swift
+//  HomepwnerBNR
+//
+//  Created by Anatoliy Chernyuk on 1/2/18.
+//  Copyright © 2018 Anatoliy Chernyuk. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
+}
